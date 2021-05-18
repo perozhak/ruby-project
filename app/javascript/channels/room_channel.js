3a069981721f46a6fbde866a37e28aa544230826
perozhak <vitalii.perozhak@gmail.com>
2021-05-18 03:09:22 +0300

    Added styles and online sending messages

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,41 @@
+import consumer from "./consumer"
+
+document.addEventListener('turbolinks:load', () => {
+  const room_element = document.getElementById('room-id');
+  const room_id = Number(room_element.getAttribute('data-room-id'));
+
+
+  consumer.subscriptions.subscriptions.forEach((subscription) => {
+    consumer.subscriptions.remove(subscription);
+  })
+
+  consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id}, {
+    connected() {
+      // Called when the subscription is ready for use on the server
+      console.log("connected... to "+room_id)
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      console.log(data)
+  
+      const element = document.getElementById('user-id');
+      const user_id = Number(element.getAttribute('data-user-id'));
+  
+      let html
+      if(user_id === data.message.user_id){
+        html = data.mine
+      }else{
+        html = data.theirs
+      }
+  
+      const messageContainer = document.getElementById('messages')
+      messageContainer.innerHTML = messageContainer.innerHTML + html
+    }
+  });
+  
+})
